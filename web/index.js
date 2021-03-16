85876090a4f668002c64fde3fe89bcf43a68a3e9
Michael-Weger <michaelweger5053@gmail.com>
2021-03-16 15:09:09 -0400

    Added new webpages for different item listings. Added pages to /web/index.js

--- web/index.js
+++ web/index.js
@@ -14,6 +14,18 @@ app.get('/login', function (req, res) {
 app.get('/splash', function (req, res) {
   res.sendFile(path.join(__dirname + '/webpages/splash.html'));
 })
+
+app.get('/merchandise', function (req, res) {
+  res.sendFile(path.join(__dirname + '/webpages/merchandise.html'));
+})
+
+app.get('/parts', function (req, res) {
+  res.sendFile(path.join(__dirname + '/webpages/parts.html'));
+})
+
+app.get('/motorcycles', function (req, res) {
+  res.sendFile(path.join(__dirname + '/webpages/motorcycles.html'));
+})
  
 app.listen(80)
 console.log("Website Launched on port 80");
